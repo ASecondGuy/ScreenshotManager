9f2361b8bb2251e7ea40fd634897fbd157fb2437
ASecondGuy <asecondguy@gmail.com>
2025-03-26 22:11:32 +0100

    screenshot selector consistent sized screenshots

--- addons/screenshotmanager/selector/screenshot_selector.gd
+++ addons/screenshotmanager/selector/screenshot_selector.gd
@@ -1,7 +1,7 @@
 class_name ScreenshotSelector
 extends Window
 
-signal selected(path:String, image:Image, texture:ImageTexture)
+signal selected(path:String, image:Image)
 
 @onready var _flow = $SC/HF
 
@@ -14,22 +14,26 @@ func _clear():
 	hide()
 
 func start_selection():
+	popup_centered_ratio()
 	var sh : Array = ScreenshotManager.get_all_screenshots()
+	#desired screenshot width is 20% of the screen
+	var sh_width : float = get_window().size.x*0.2
 	for shot in sh:
 		var b := TextureButton.new()
 		_flow.add_child(b)
 		b.size_flags_horizontal = b.SIZE_EXPAND_FILL
 		b.stretch_mode = b.STRETCH_KEEP_ASPECT_CENTERED
 		var texture := ImageTexture.create_from_image(shot["image"])
-		texture.set_size_override(shot["image"].get_size()*0.20)
+		# how much to resize so the screenshot has wanted width
+		var mult : float = sh_width/shot["image"].get_size().x
+		texture.set_size_override(shot["image"].get_size()*mult)
 		b.texture_normal = texture
-		b.pressed.connect(_on_selected.bind(shot, texture))
-	popup_centered_ratio()
+		b.pressed.connect(_on_selected.bind(shot))
 
 
-func _on_selected(data:Dictionary, texture:ImageTexture):
+func _on_selected(data:Dictionary):
 	_clear()
-	selected.emit(data["path"], data["image"], texture)
+	selected.emit(data["path"], data["image"])
 
 
 func _on_close_requested():
